c26980c02dc1f7aa52a18bdf7e19a31e6a71ab5a
Daniel <daniel.lub2007@gmail.com>
2024-02-13 22:00:28 +0200

    4

--- app/src/main/java/com/example/daniellublinskyedittext/MainActivity.java
+++ app/src/main/java/com/example/daniellublinskyedittext/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.daniellublinskyedittext;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Button;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    TextView one;
+    TextView two;
+    TextView three;
+    TextView four;
+    TextView five;
+    TextView six;
+    EditText a;
+    EditText b;
+    EditText c;
+    ImageView I;
+    ImageView II;
+    ImageView III;
+    Button odin;
+    Button dva;
+    Button tri;
 
+    Button chitiri;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        a = findViewById(R.id.a);
+        b = findViewById(R.id.b);
+        c = findViewById(R.id.c);
+        I = findViewById(R.id.I);
+        II = findViewById(R.id.II);
+        III = findViewById(R.id.III);
+        odin = findViewById(R.id.odin);
+        dva = findViewById(R.id.dva);
+        tri = findViewById(R.id.tri);
+        chitiri = findViewById(R.id.chitiri);
     }
 }
